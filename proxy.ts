b1fbc17f922770d680a009562e7ed76695c3b7de
Lucas Maciel <lucasmacielcontato@gmail.com>
2025-11-27 11:34:54 -0300

    fix: protect all routes, except for root, assets and auth/

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,5 @@
-
-console.log("🔥 PROXY ATIVADO");
-
 export { auth as proxy } from "@/auth";
 
 export const config = {
-  matcher: ["/api/trashspots", "/api/trashspots/:path*"],
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico|$).*)"],
 };
